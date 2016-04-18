a9bb4055278a1c4d71bf723cbed5e06128e3b1b9
Blake Polidore <blakepolidore@gmail.com>
2016-04-18 13:03:43 -0700

    Deleted ShareLinkContent model

--- app/src/main/java/adi/sf1/targaryen/newyorktimes/FacebookActivity.java
+++ app/src/main/java/adi/sf1/targaryen/newyorktimes/FacebookActivity.java
@@ -1,10 +1,12 @@
 package adi.sf1.targaryen.newyorktimes;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.facebook.FacebookSdk;
+import com.facebook.share.model.ShareLinkContent;
 
 /**
  * Created by Raiders on 4/18/16.
@@ -17,7 +19,12 @@ public class FacebookActivity extends AppCompatActivity {
 
         //Initialize Facebook SDK
         FacebookSdk.sdkInitialize(getApplicationContext());
+    }
 
+    public void setFacebookContent(String url) {
+        ShareLinkContent shareLinkContent = new ShareLinkContent.Builder()
+          .setContentUrl(Uri.parse(url))
+          .build();
     }
 
 
