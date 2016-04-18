f31ce4073748158d80101ed83a2a8cdb90d41ca7
Blake Polidore <blakepolidore@gmail.com>
2016-04-18 11:54:32 -0700

    Added Facebook activity, facebook to the manifest and added facebook id to strings file

---
+++ app/src/main/java/adi/sf1/targaryen/newyorktimes/FacebookActivity.java
@@ -0,0 +1,21 @@
+package adi.sf1.targaryen.newyorktimes;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.facebook.FacebookSdk;
+
+/**
+ * Created by Raiders on 4/18/16.
+ */
+public class FacebookActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+        //Initialize Facebook SDK
+        FacebookSdk.sdkInitialize(getApplicationContext());
+    }
+}
