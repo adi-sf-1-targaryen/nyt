701b7770b2f33f26aa6d571048bf305d75c7fc7c
Nick Saad <nicolassaad1984@gmail.com>
2016-04-19 12:12:12 -0700

    Connected APIs to fragments

--- app/src/main/java/adi/sf1/targaryen/newyorktimes/fragment/ArticleFeedFragment.java
+++ app/src/main/java/adi/sf1/targaryen/newyorktimes/fragment/ArticleFeedFragment.java
@@ -9,24 +9,38 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import java.util.ArrayList;
-import java.util.List;
+import android.widget.Toast;
 
 import adi.sf1.targaryen.newyorktimes.R;
-import adi.sf1.targaryen.newyorktimes.model.ArticleFeed;
+import adi.sf1.targaryen.newyorktimes.api.NewYorkTimes;
+import adi.sf1.targaryen.newyorktimes.api.TopStories;
 import adi.sf1.targaryen.newyorktimes.recyclerAdapter.ArticleFeedAdapter;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by Raiders on 4/18/16.
  */
 public class ArticleFeedFragment extends Fragment{
 
-  private Context context;
-  private List<ArticleFeed> feedList = new ArrayList<>();
+  protected Context context;
+//  private List<ArticleFeed> feedList = new ArrayList<>();
   private RecyclerView recyclerView;
-  private ArticleFeedAdapter articleFeedAdapter;
+  protected ArticleFeedAdapter articleFeedAdapter;
+  public static final String EXTRA_SECTION = "section";
+  private TopStories.Section section = TopStories.Section.HOME;
 
+  @Override
+  public void onCreate(@Nullable Bundle savedInstanceState) {
+    Bundle bundle = getArguments();
+    if (bundle != null) {
+      String section = bundle.getString(EXTRA_SECTION);
+      if (section != null) {
+        this.section = TopStories.Section.valueOf(section);
+      }
+    }
+  }
 
   @Nullable
   @Override
@@ -34,20 +48,31 @@ public class ArticleFeedFragment extends Fragment{
     View view = inflater.inflate(R.layout.fragment_feed, container, false);
     recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
     context = getContext();
-    setFeedList();
     setArticleFeedAdapter();
+    setFeedList();
     return view;
   }
   private void setArticleFeedAdapter() {
-    articleFeedAdapter = new ArticleFeedAdapter(feedList);
+    articleFeedAdapter = new ArticleFeedAdapter(/*feedList*/);
     RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(context);
     recyclerView.setLayoutManager(layoutManager);
     recyclerView.setAdapter(articleFeedAdapter);
 
   }
 
-  private void setFeedList() {
-    //Where the api calls happen
+  protected void setFeedList() {
+    NewYorkTimes.getInstance().getTopStories(section).enqueue(new Callback<TopStories>() {
+      @Override
+      public void onResponse(Call<TopStories> call, Response<TopStories> response) {
+        articleFeedAdapter.changeDataSet(response.body().getResults());
+      }
+
+      @Override
+      public void onFailure(Call<TopStories> call, Throwable t) {
+        Toast.makeText(context, "Could not retrieve Top Stories", Toast.LENGTH_SHORT).show();
+      }
+    });
+
 
   }
 }
---
+++ app/src/main/java/adi/sf1/targaryen/newyorktimes/fragment/MostPopularFeedFragment.java
@@ -0,0 +1,38 @@
+package adi.sf1.targaryen.newyorktimes.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+import adi.sf1.targaryen.newyorktimes.api.MostPopular;
+import adi.sf1.targaryen.newyorktimes.api.NewYorkTimes;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * Created by nicolassaad on 4/19/16.
+ */
+public class MostPopularFeedFragment extends ArticleFeedFragment {
+
+
+  @Override
+  public void onCreate(@Nullable Bundle savedInstanceState) {
+    // TODO: 4/19/16 DO STUFF HERE MAYBE (MAYBE NOT)
+  }
+
+  @Override
+  protected void setFeedList() {
+    NewYorkTimes.getInstance().getMostPopular(MostPopular.Type.VIEWED).enqueue(new Callback<MostPopular>() {
+      @Override
+      public void onResponse(Call<MostPopular> call, Response<MostPopular> response) {
+        articleFeedAdapter.changeDataSet(response.body().getResults());
+      }
+
+      @Override
+      public void onFailure(Call<MostPopular> call, Throwable t) {
+        Toast.makeText(context, "Could not retrieve Most Popular Stories", Toast.LENGTH_SHORT).show();
+      }
+    });
+  }
+}
