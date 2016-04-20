5487f4b39362fed310bb36b48f9509648ffdf3b1
Nick Saad <nicolassaad1984@gmail.com>
2016-04-20 13:43:26 -0700

    Commmitting new changes to the toolbar settings, more work needed

--- app/src/main/java/adi/sf1/targaryen/newyorktimes/MainActivity.java
+++ app/src/main/java/adi/sf1/targaryen/newyorktimes/MainActivity.java
@@ -1,6 +1,5 @@
 package adi.sf1.targaryen.newyorktimes;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -51,7 +50,6 @@ public class MainActivity extends AppCompatActivity implements ActionBar.TabList
 
   }
 
-
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
 // Inflate the menu; this adds items to the action bar if it is present.
@@ -66,11 +64,11 @@ public class MainActivity extends AppCompatActivity implements ActionBar.TabList
 // Handle action bar item clicks here. The action bar will
 // automatically handle clicks on the Home/Up button, so long
 // as you specify a parent activity in AndroidManifest.xml.
- 
+
     switch (item.getItemId()) {
       case R.id.preferences:
-        Intent preferencesIntent = new Intent(MainActivity.this, NotificationPreferencesActivity.class);
-        startActivity(preferencesIntent);
+//        Intent preferencesIntent = new Intent(MainActivity.this, NotificationPreferencesActivity.class);
+//        startActivity(preferencesIntent);
         return true;
     }
 
