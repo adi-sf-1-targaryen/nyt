f90eb33c5d147bcf2c3de0cf820a2f48ac22f003
Matthew Oltendorf <matthew@oltendorf.net>
2016-04-21 15:57:30 -0700

    Removed overridden method causing crash in search fragment

--- app/src/main/java/adi/sf1/targaryen/newyorktimes/fragment/SearchFragment.java
+++ app/src/main/java/adi/sf1/targaryen/newyorktimes/fragment/SearchFragment.java
@@ -1,18 +1,11 @@
 package adi.sf1.targaryen.newyorktimes.fragment;
 
-import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.util.Log;
 import android.widget.Toast;
 
 import adi.sf1.targaryen.newyorktimes.MainActivity;
-import adi.sf1.targaryen.newyorktimes.R;
 import adi.sf1.targaryen.newyorktimes.api.Call;
 import adi.sf1.targaryen.newyorktimes.api.Callback;
 import adi.sf1.targaryen.newyorktimes.api.NewYorkTimes;
@@ -27,7 +20,6 @@ import retrofit2.Response;
 public class SearchFragment extends ArticleFeedFragment {
   private static final String TAG = "SearchFragment";
 
-  RecyclerView recyclerViewSearch;
   String searchArticleQuery;
 
   @Override
@@ -37,29 +29,6 @@ public class SearchFragment extends ArticleFeedFragment {
     if (bundle != null) {
       searchArticleQuery = bundle.getString(MainActivity.SEARCH_KEY);
     }
-  }
-
-  @Nullable
-  @Override
-  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-    View view = inflater.inflate(R.layout.fragment_search, container, false);
-    recyclerViewSearch = (RecyclerView) view.findViewById(R.id.search_recycler_view);
-    swipeContainer = (SwipeRefreshLayout) view.findViewById(R.id.swipeContainer);
-    context = getContext();
-    setArticleFeedAdapter();
-    setFeedList();
-    swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-      @Override
-      public void onRefresh() {
-        setFeedList(false);
-      }
-    });
-    // Configure the refreshing colors
-    swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
-      android.R.color.holo_green_light,
-      android.R.color.holo_orange_light,
-      android.R.color.holo_red_light);
-    return view;
   }
 
   @Override
